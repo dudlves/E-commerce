6d20475522025fc02155048ab8da9005bc43b5fd
dudlves <dudlves17@gmail.com>
2022-12-12 08:59:52 -0300

    sqlite3

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require('express')
 const app = express();
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('Hello Word')
 })
 
-// app.post('/usuario', (req, res) => {
-//     insertUsuario(req.body);
-//     res.json({
-//         "statucCode": 200
-//     })
-// })
+app.post('/usuario', (req, res) => {
+   console.log(req.body)
+    res.json({
+        "statucCode": 200
+    })
+})
 
 app.listen(3000);
