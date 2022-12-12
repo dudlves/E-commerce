ab6e8bb167481e3a5573568c31dc77738afdd0c1
dudlves <dudlves17@gmail.com>
2022-12-12 08:31:54 -0300

    nodemon

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("OPA")
+const express = require('express')
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello Word')
+})
+
+// app.post('/usuario', (req, res) => {
+//     insertUsuario(req.body);
+//     res.json({
+//         "statucCode": 200
+//     })
+// })
+
+app.listen(3000);
